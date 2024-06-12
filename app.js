4f4253b074828efad2879bea131ec84c62d26379
liue853224@gmail.com <liue853224@gmail.com>
2024-06-12 14:10:07 +0800

    Set view engine and hbs base page

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
+// 定義
 const express = require("express");
+const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+// 設置
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", "./views");
 app.use(express.static("public"));
 
+// 路由
 app.get("/", (req, res) => {
   res.redirect("/movies");
 });
 
 app.get("/movies", (req, res) => {
-  res.send("Listing movies");
+  res.render("index");
 });
 
 app.get("/movie/:id", (req, res) => {
   const id = req.params.id;
   res.send(`read movie:${id}`);
 });
-
+// Listening
 app.listen(port, () => {
   console.log(`express server is running on http://localhost${port}`);
 });
